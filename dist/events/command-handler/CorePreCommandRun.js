af527d3f92ecf24f713c08fd314cb6da6be49797
Favna <contact@skyra.pw>
2020-10-10 19:32:38 +0000

    build: tsc build for a24175ca138fa552fb5a250389e83a5206c4db34

--- dist/events/command-handler/CorePreCommandRun.js
+++ dist/events/command-handler/CorePreCommandRun.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CoreEvent = void 0;
+const UserError_1 = require("../../lib/errors/UserError");
 const Event_1 = require("../../lib/structures/Event");
 const Events_1 = require("../../lib/types/Events");
 const Result_1 = require("../../lib/utils/Result");
@@ -9,6 +10,15 @@ class CoreEvent extends Event_1.Event {
         super(context, { event: Events_1.Events.PreCommandRun });
     }
     async run({ message, command, parameters, commandName, prefix }) {
+        if (!command.enabled) {
+            this.client.emit(Events_1.Events.CommandDenied, new UserError_1.UserError('CommandDisabled', 'This command is disabled.'), {
+                message,
+                command,
+                parameters,
+                commandName,
+                prefix
+            });
+        }
         const result = await command.preconditions.run(message, command);
         if (Result_1.isErr(result)) {
             this.client.emit(Events_1.Events.CommandDenied, result.error, { message, command, parameters, commandName, prefix });
